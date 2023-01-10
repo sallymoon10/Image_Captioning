a1ff6150395836eb251a2f69add6ca9317f7de90
Sally Moon <sallymoon@192.168.1.130>
2023-01-10 21:05:51 +0000

    added logging logits, gradients. running on a couple epochs to test

--- src/models/vit_gpt2.py
+++ src/models/vit_gpt2.py
@@ -45,18 +45,23 @@ class VitGpt2(LightningModule):
         self.val_loss_best = MinMetric()
 
     def set_up_model_for_training(self):
-        '''Only unfreeze last N layers of encoder and decoder. To fine-tune small number of layers under low resource setting.'''
+        '''Only unfreeze last layer of encoder and decoder. To fine-tune small number of layers under low resource setting.'''
         self.model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
         self.feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
         self.tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
         freeze_model_and_make_eval(self.model)
-        
-        self.unfreeze_from_layer_i(module = self.model.encoder, layer_i= len(list(self.model.encoder.parameters())) - self.num_layers_to_train - 1)
-        self.unfreeze_from_layer_i(module = self.model.decoder, layer_i= len(list(self.model.decoder.parameters())) - self.num_layers_to_train - 1)
+        self.unfreeze_last_layers()
 
-    def unfreeze_from_layer_i(self, module, layer_i: int):
-        for i, param in enumerate(module.parameters()):
-            if i >= layer_i:
+    def unfreeze_last_layers(self):
+        '''
+        Unfreeze layer layer of encoder and decoder
+        - Manually identified last layers to unfreeze for demonstration purposes
+        '''
+        encoder_last_layer = ['model.encoder.encoder.layer.11.attention.attention.query.bias', 'model.encoder.encoder.layer.11.attention.attention.key.weight', 'model.encoder.encoder.layer.11.attention.attention.key.bias', 'model.encoder.encoder.layer.11.attention.attention.value.weight', 'model.encoder.encoder.layer.11.attention.attention.value.bias', 'model.encoder.encoder.layer.11.attention.output.dense.weight', 'model.encoder.encoder.layer.11.attention.output.dense.bias', 'model.encoder.encoder.layer.11.intermediate.dense.weight', 'model.encoder.encoder.layer.11.intermediate.dense.bias', 'model.encoder.encoder.layer.11.output.dense.weight', 'model.encoder.encoder.layer.11.output.dense.bias', 'model.encoder.encoder.layer.11.layernorm_before.weight', 'model.encoder.encoder.layer.11.layernorm_before.bias', 'model.encoder.encoder.layer.11.layernorm_after.weight', 'model.encoder.encoder.layer.11.layernorm_after.bias']
+        decoder_last_layer = ['model.decoder.transformer.h.11.ln_1.weight', 'model.decoder.transformer.h.11.ln_1.bias', 'model.decoder.transformer.h.11.attn.c_attn.weight', 'model.decoder.transformer.h.11.attn.c_attn.bias', 'model.decoder.transformer.h.11.attn.c_proj.weight', 'model.decoder.transformer.h.11.attn.c_proj.bias', 'model.decoder.transformer.h.11.ln_2.weight', 'model.decoder.transformer.h.11.ln_2.bias', 'model.decoder.transformer.h.11.crossattention.c_attn.weight', 'model.decoder.transformer.h.11.crossattention.c_attn.bias', 'model.decoder.transformer.h.11.crossattention.q_attn.weight', 'model.decoder.transformer.h.11.crossattention.q_attn.bias', 'model.decoder.transformer.h.11.crossattention.c_proj.weight', 'model.decoder.transformer.h.11.crossattention.c_proj.bias', 'model.decoder.transformer.h.11.ln_cross_attn.weight', 'model.decoder.transformer.h.11.ln_cross_attn.bias', 'model.decoder.transformer.h.11.mlp.c_fc.weight', 'model.decoder.transformer.h.11.mlp.c_fc.bias', 'model.decoder.transformer.h.11.mlp.c_proj.weight', 'model.decoder.transformer.h.11.mlp.c_proj.bias']
+
+        for name, param in dict(self.named_parameters()).items():
+            if name in encoder_last_layer or name in decoder_last_layer:
                 param.requires_grad = True
         
     def configure_optimizers(self):
@@ -97,19 +102,30 @@ class VitGpt2(LightningModule):
         caption_target_ids = caption_target_ids.to(self.device)
 
         # generate output logits
-        loss = self.model(pixel_values=pixel_values, labels=caption_target_ids).loss
+        output = self.model(pixel_values=pixel_values, labels=caption_target_ids)
+        loss = output.loss
+        logits = output.logits
 
         # generate caption
         output_ids = self.model.generate(pixel_values, **self.gen_kwargs)
         output_caption = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
 
-        return {"loss": loss, "output_caption": output_caption}
+        return {"loss": loss, "output_caption": output_caption, "logits": logits}
     
+    def on_after_backward(self):
+        global_step = self.global_step
+        for name, param in self.named_parameters():
+            if param.requires_grad and param.grad is not None:
+                abs_mean_grad = abs(param.grad).mean()
+                self.logger.experiment.add_histogram(f"{name}_grad", param.grad, global_step)
+                self.logger.experiment.add_scalar(f"{name}_abs_mean_grad", abs_mean_grad, global_step)
+
     def training_step(self, batch: Any, batch_idx: int):
         output = self(batch)
 
         self.train_loss(output["loss"])
         self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log_logits(logits=output["logits"], name="train/logits")
         return output
 
     def training_epoch_end(self, outputs: List[Any]):
@@ -120,7 +136,7 @@ class VitGpt2(LightningModule):
         # update and log metrics
         self.val_loss(output["loss"])
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True)
-
+        self.log_logits(logits=output["logits"], name="val/logits")
         return output
     
     def validation_epoch_end(self, outputs: List[Any]):
@@ -133,9 +149,14 @@ class VitGpt2(LightningModule):
 
         self.test_loss(output["loss"])
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log_logits(logits=output["logits"], name="test/logits")
         return output
     
     def log_params_as_histogram(self):
         '''Save weight histogram to tensorboard'''
         for name, params in self.named_parameters():
             self.logger.experiment.add_histogram(name, params, self.current_epoch)
+    
+    def log_logits(self, logits: torch.Tensor, name: str):
+        '''Save logit histogram to tensorboard'''
+        self.logger.experiment.add_histogram(name, logits, self.current_epoch)
